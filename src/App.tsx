15d07f75c459529efbe21355aa6cf052363f1c0e
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2021-08-06 11:28:10 +0200

    first style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import {Box, Container} from "@material-ui/core";
+import FloatContainer from "./components/containers/FloatContainer";
 
 function App() {
     return (
         <div>
-            <div>
-                <h1 style={{color: 'black'}}>
-                    NSVB
-                </h1>
+            <div style={{position: "absolute", left: 0, right: 0}}>
+                <Container>
+                    <Box display='flex' alignItems='center'>
+                        <Box pt={3} pb={3}>
+                            <h2 style={{color: 'white', margin: 0}}>
+                                NSVB
+                            </h2>
+                        </Box>
+                        <Box flex={1}/>
+                        <Box style={{color: 'white'}}>
+                            <p>
+                                t
+                            </p>
+                        </Box>
+                    </Box>
+
+                </Container>
             </div>
-            
+            <div style={{height: 400, background: '#273A6B'}}>
+            </div>
+            <Container>
+                <div style={{position: 'relative'}}>
+                    <div style={{position: "absolute", left: 0, right: 0, top: -32, zIndex: 10}}>
+                        <FloatContainer>
+                            Picker
+                        </FloatContainer>
+                    </div>
+                </div>
+            </Container>
         </div>
     );
 }
---
+++ src/components/containers/FloatContainer.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+
+export const FloatContainer = styled.div`
+background: white;  
+height: 100%;
+width: 100%;
+position: relative;
+padding: 32px;
+border-radius: 16px;
+box-shadow:  0px 16px 40px rgba(112, 144, 176,0.2);
+`;
+
+export default FloatContainer;
