e140e36a411703ae179adcfeaebcd7d0dfa38398
NicolaiLassen <nicolai.m.t.lassen@gmail.com>
2021-10-05 11:48:58 +0200

    image hover effect

--- src/components/cards/ArticleCard.tsx
+++ src/components/cards/ArticleCard.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-import {Paper} from "@material-ui/core";
+import {makeStyles, Paper, Theme} from "@material-ui/core";
 
+interface ArticleCardStyleProps {
+    img: string;
+}
+
+const useStyles = makeStyles<Theme, ArticleCardStyleProps>((theme) => ({
+    img: {
+        backgroundImage: ({img}) => `url(${img})`,
+        width: '100%',
+        height: '100%',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        backgroundSize: '100% 100%',
+        transition: 'all 0.2s ease',
+        '&:hover': {
+            transform: 'scale(1.1)'
+        }
+    }
+}));
 
 interface ArticleCardProps {
     href: string;
@@ -8,21 +26,19 @@ interface ArticleCardProps {
 }
 
 const ArticleCard: React.FC<ArticleCardProps> = ({href, img}) => {
+    const classes = useStyles({img});
     return (
         <a target='_blank' rel="noreferrer"
            href={href}>
             <Paper
                 elevation={2}
                 style={{
-                    backgroundImage: `url(${img})`,
                     height: 400,
                     width: '100%',
-                    boxShadow: '0px 8px 30px rgb(112 144 176 / 30%)',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center',
-                    backgroundSize: 'cover'
+                    boxShadow: '0px 8px 30px rgb(112 144 176 / 30%)'
                 }}
             >
+                <div className={classes.img}/>
             </Paper>
         </a>
     )
